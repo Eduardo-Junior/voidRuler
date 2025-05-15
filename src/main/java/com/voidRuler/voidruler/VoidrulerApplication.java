36583ab292e57faa687b7071d00fa8da09bac632
Eduardo <drdandre@gmail.com>
2025-05-15 15:55:06 -0300

    Commit inicial

---
+++ src/main/java/com/voidRuler/voidruler/VoidrulerApplication.java
@@ -0,0 +1,13 @@
+package com.voidRuler.voidruler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoidrulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoidrulerApplication.class, args);
+	}
+
+}
