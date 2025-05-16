8ce8fcbb992ce65464759544007b2f0e2a5d4699
Eduardo <drdandre@gmail.com>
2025-05-16 15:16:22 -0300

    Fix: Changed ID type from int to Long
    Feature: Ruler class created and added to Empire. (Representation of players)

--- src/main/java/com/voidRuler/voidruler/entities/Empire.java
+++ src/main/java/com/voidRuler/voidruler/entities/Empire.java
@@ -6,24 +6,34 @@ import jakarta.persistence.*;
 public class Empire {
     @Id
     @GeneratedValue
-    private int id;
+    private Long id;
 
     private String name;
 
     private int population;
     private int techLevel;
+    private int scienceOutput;
+    private int militaryPower;
+    private String governmentType;
+
+    @OneToOne(mappedBy = "empire", cascade = CascadeType.ALL)
+    private Ruler ruler;
+
 
     public Empire() {
         super();
     }
 
-    public Empire(String name, int population, int techLevel) {
+    public Empire(String name, int population, int techLevel, int scienceOutput, int militaryPower, String governmentType) {    
         this.name = name;
         this.population = population;
         this.techLevel = techLevel;
+        this.scienceOutput = scienceOutput;
+        this.militaryPower = militaryPower;
+        this.governmentType = governmentType;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
@@ -50,4 +60,38 @@ public class Empire {
     public void setTechLevel(int techLevel) {
         this.techLevel = techLevel;
     }
+
+    public int getScienceOutput() {
+        return scienceOutput;
+    }
+
+    public void setScienceOutput(int scienceOutput) {
+        this.scienceOutput = scienceOutput;
+    }
+
+    public int getMilitaryPower() {
+        return militaryPower;
+    }
+
+    public void setMilitaryPower(int militaryPower) {
+        this.militaryPower = militaryPower;
+    }
+
+    public String getGovernmentType() {
+        return governmentType;
+    }
+
+    public void setGovernmentType(String governmentType) {
+        this.governmentType = governmentType;
+    }
+
+    public Ruler getRuler() {
+        return ruler;
+    }
+
+    public void setRuler(Ruler ruler) {
+        this.ruler = ruler;
+    }
+
+    
 }
